f8a8dec4ffb6d748d0b6dc06c027251ca71998a8
chiperman <chipermanju@gmail.com>
2023-12-15 00:37:29 +0800

    feat(user): 新增 JWT 用户验证

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // 用户注册路由
 router.post('/register', async (req, res) => {
@@ -64,19 +66,64 @@ router.post('/login', async (req, res) => {
       });
     }
 
+    const secretKey = process.env.SECRET_KEY;
+
+    // 登录成功生成 JWT
+    const token = jwt.sign({ userId: user._id, email: user.email }, secretKey, {
+      expiresIn: '24h',
+    });
+
     // 如果登录成功，返回用户的 id、username、email
     res.status(200).json({
       code: 200,
       message: '登录成功',
       data: {
+        token,
         id: user._id,
         username: user.username,
         email: user.email,
       },
     });
   } catch (error) {
-    res.status(500).send('登录失败');
+    res.status(500).json({
+      code: 500,
+      message: '服务器错误',
+      data: null,
+    });
   }
+});
+
+// 验证用户 token
+router.post('/verifyToken', (req, res) => {
+  const token = req.body.token;
+
+  if (!token) {
+    return res.status(200).json({
+      code: 404,
+      message: '未提供 token',
+      data: { valid: false },
+    });
+  }
+
+  const secretKey = process.env.SECRET_KEY;
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      console.error('Token 验证失败', err);
+      return res.status(200).json({
+        code: 403,
+        message: 'Token 验证失败',
+        data: { valid: false },
+      });
+    }
+
+    console.log('Token 验证成功', decoded);
+    res.status(200).json({
+      code: 200,
+      message: 'Token 验证成功',
+      data: { valid: true, decoded },
+    });
+  });
 });
 
 module.exports = router;
